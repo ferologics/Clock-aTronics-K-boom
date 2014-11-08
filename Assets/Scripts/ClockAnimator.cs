b0b767f84c0fba9c8131c37106cd073b09737084
František Heteš <fhetes@gmail.com>
2014-11-08 17:08:12 +0100

    been playin around
    …

---
+++ Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockAnimator: MonoBehaviour {
+
+	public Transform hours, minutes, seconds;
+	public bool analog;
+
+	private const float
+		hoursToDegrees = 360/12,
+		minutesToDegrees = 360/60,
+		secondsToDegrees = 360/60;
+
+	void Update () {
+
+		if (analog) {
+			TimeSpan time = DateTime.Now.TimeOfDay;
+			hours.localRotation = Quaternion.Euler (0f, 0f, (float)time.TotalHours * -hoursToDegrees);
+			minutes.localRotation = Quaternion.Euler (0f, 0f, (float)time.TotalMinutes * -minutesToDegrees);
+			seconds.localRotation = Quaternion.Euler (0f, 0f, (float)time.TotalSeconds * -secondsToDegrees);
+
+		} else {
+
+			DateTime time = DateTime.Now;
+			hours.localRotation = Quaternion.Euler (0f, 0f, time.Hour * -hoursToDegrees);
+			minutes.localRotation = Quaternion.Euler (0f, 0f, time.Minute * -minutesToDegrees);
+			seconds.localRotation = Quaternion.Euler (0f, 0f, time.Second * -secondsToDegrees);
+		}
+	}
+}
